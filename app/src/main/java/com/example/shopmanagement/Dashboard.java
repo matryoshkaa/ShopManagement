22456ffc227a2e811725e0ec185cf81e347dd085
matryoshkaa <66214321+matryoshkaa@users.noreply.github.com>
2020-11-17 12:44:06 +0400

    Settings drawer complete

--- app/src/main/java/com/example/shopmanagement/Dashboard.java
+++ app/src/main/java/com/example/shopmanagement/Dashboard.java
@@ -1,15 +1,40 @@
 package com.example.shopmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class Dashboard extends Activity {
+public class Dashboard extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        @SuppressLint("UseCompatLoadingForDrawables") final Drawable settings = getResources().getDrawable(R.drawable.settings_icon);
+        getSupportActionBar().setHomeAsUpIndicator(settings);
+
+        //settings button
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent();
+                startActivity(new Intent(Dashboard.this, Settings.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/shopmanagement/Settings.java
@@ -0,0 +1,60 @@
+package com.example.shopmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Settings extends AppCompatActivity {
+
+    Toolbar toolbar;
+    FirebaseAuth mFirebaseAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        @SuppressLint("UseCompatLoadingForDrawables") final Drawable goBack = getResources().getDrawable(R.drawable.back_button);
+        getSupportActionBar().setHomeAsUpIndicator(goBack);
+
+        //go back button
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public void goToTermsAndCondition (View view){
+        startActivity(new Intent(this, TermsAndCondition.class));
+    }
+
+    public void goToFeedbackAndSupport (View view){
+        startActivity(new Intent(this, FeedbackAndSupport.class));
+    }
+
+    public void logout (View view){
+        mFirebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(this, SignIn.class));
+
+    }
+
+
+}
